cc06002dfc60c9c4046199a3521dac6a817ddc59
BenBeebs <cameronts01@gmail.com>
2017-04-18 14:23:29 -0400

    nee

---
+++ Roll Ball/Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    float score = 0;
+    public Text scoreText;
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+    //modify score when collides with pickup
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+            score = score + 10;
+        scoreText.text = "score:" + score;
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
